81d20040063ef76b3c4be35bcf1cb99f7ec45ebc
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-04-22 12:19:40 +0300

    dump commit

--- site/js/scroll-detector-for-header.js
+++ site/js/scroll-detector-for-header.js
@@ -1,11 +1,27 @@
-document.addEventListener('scroll', () => {
+document.addEventListener('DOMContentLoaded', function () {
+  resolveHeadersClasses()
+
+  document.addEventListener('scroll', () => {
+    resolveHeadersClasses()
+  })
+})
+
+function resolveHeadersClasses() {
   const header = document.getElementById('navigation')
+  const background = 'background__white'
+  const shadow = 'bottom-shadow'
+
+  if(!header) return
 
   if(window?.scrollY && window.scrollY > 0) {
-    header.classList.add('background__white')
-    header.classList.add('bottom-shadow')
+    if(header.classList.contains(background)) return
+
+    header.classList.add(background)
+    header.classList.add(shadow)
   } else {
-    header.classList.remove('background__white')
-    header.classList.remove('bottom-shadow')
+    if(!header.classList.contains(background)) return
+
+    header.classList.remove(background)
+    header.classList.remove(shadow)
   }
-})
+}
